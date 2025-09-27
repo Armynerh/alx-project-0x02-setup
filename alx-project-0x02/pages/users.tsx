3bffe844255a2d448e08779f9eb17527bf53242c
Armynerh <aimenah@outlook.com>
2025-09-27 14:09:50 -0400

    Create users page

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,5 +1,5 @@
-import { UserProps } from "@/interfaces";
 import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
 
 interface UsersPageProps {
   users: UserProps[];
@@ -9,7 +9,6 @@ const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <div className="max-w-4xl mx-auto px-4 py-6">
       <h1 className="text-2xl font-bold mb-6">Users</h1>
-
       <div className="grid gap-4 md:grid-cols-2">
         {users.map((user) => (
           <UserCard key={user.id} {...user} />
@@ -21,7 +20,7 @@ const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await res.json();
+  const users: UserProps[] = await res.json();
 
   return {
     props: {
