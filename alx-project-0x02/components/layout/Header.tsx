6befcd5864cdcb884738359915f285a374c22082
Armynerh <aimenah@outlook.com>
2025-09-27 00:11:36 -0400

    Create basic routing

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,10 @@
+
 const Header: React.FC = () => {
   return (
     <header>
       <h1>My Application Header</h1>
       <nav>
-        <a href="/">Home</a> | <a href="/about">About</a> | <a href="/contact">Contact</a>
+        <a href="/home">Home</a> | <a href="/about">About</a> | <a href="/contact">Contact</a>
       </nav>
     </header>
   );
---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,8 @@
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+    </div>
+  );
+};
+export default About;
